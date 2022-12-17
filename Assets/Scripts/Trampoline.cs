ea0ab8075f25593269ff8c9ca1bb580814cfd68c
yznxsg123 <1103065464@qq.com>
2022-12-17 16:07:44 +0800

    game_yzn_1

---
+++ Assets/Scripts/Trampoline.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trampoline : MonoBehaviour
+{
+    public float jumpForce = 22;
+
+    private Animator _animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter2D(Collision2D other) 
+    {
+        if (other.gameObject.tag == "Player") 
+        {
+            _animator.SetTrigger(name: "jump");
+            other.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(x: 0, y: jumpForce), ForceMode2D.Impulse);
+        }
+    }
+}
